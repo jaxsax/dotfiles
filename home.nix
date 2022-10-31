dcf3a5f17ac5279847401ac8c73b7bfefdcfd68c
jaxsax <jaxsax@users.noreply.github.com>
2022-11-01 00:08:55 +0800

    Manage git configs from home manager

--- home.nix
+++ home.nix
@@ -4,4 +4,14 @@
   home.packages = [ pkgs.nixfmt pkgs.neovim ];
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "jaxsax";
+    userEmail = "jaxsax@users.noreply.github.com";
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+    };
+  };
 }
