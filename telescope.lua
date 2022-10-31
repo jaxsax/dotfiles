9811460c46c2742ddaa7e968582c01816ff968b4
jaxsax <jaxsax@users.noreply.github.com>
2022-11-01 03:30:30 +0800

    Remap telescope keybinds

--- telescope.lua
+++ telescope.lua
@@ -2,5 +2,5 @@ require('telescope').setup{}
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+vim.keymap.set('n', '<M-f>', builtin.find_files, {})
+vim.keymap.set('n', '<M-g>', builtin.live_grep, {})
