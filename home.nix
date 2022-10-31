51078dc2ebc1bd3922f272437b516c0a0237679b
jaxsax <jaxsax@users.noreply.github.com>
2022-11-01 00:00:25 +0800

    Format files

--- home.nix
+++ home.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }: {
   home.username = "jaxsax";
   home.homeDirectory = "/home/jaxsax";
+  home.packages = [ pkgs.nixfmt pkgs.neovim ];
   home.stateVersion = "22.11";
   programs.home-manager.enable = true;
 }
